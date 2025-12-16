4f6641e5e83d9cfb49015deb3e7295a5fadae180
mhornsey-huboo <113708718+mhornsey-huboo@users.noreply.github.com>
2025-12-16 09:36:15 +0000

    Got linting working for this version of the code/packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,21 @@
-import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // Not needed with React 17+
+    },
+  },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css" },
 ]);
